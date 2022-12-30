5bffa0f54f52923ebd026dd6d713421c644c261f
YeonJun Cho <micayel1219@gmail.com>
2022-12-30 19:08:17 +0900

    refactor : edit key generator string

--- StepStone/src/main/java/com/likelion/stepstone/chat/redis/RedisKeyGenerator.java
+++ StepStone/src/main/java/com/likelion/stepstone/chat/redis/RedisKeyGenerator.java
@@ -3,10 +3,10 @@ package com.likelion.stepstone.chat.redis;
 import com.likelion.stepstone.config.CacheNames;
 
 public class RedisKeyGenerator {
-    private static final String CHAT_ROOM_KEY = "chat_room";
+    private static final String CHAT_ROOM_KEY = "CHAT_ROOM_CACHE";
     private static final String CUT_IDX = "CUT_IDX_CACHE";
     public static String generateChatRoomKey(Long chatRoomCid){
-        return CHAT_ROOM_KEY + ":" + chatRoomCid;
+        return CHAT_ROOM_KEY + "::" + chatRoomCid;
     }
 
     public static String generateChatRoomKey(String chatRoomId){
